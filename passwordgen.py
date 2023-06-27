b2537298967ef3b145b85fee4355db57180d80c0
Arlene M <84497016+arlene-08@users.noreply.github.com>
2023-06-27 03:07:02 -0500

    Create passwordgen.py

---
+++ passwordgen.py
@@ -0,0 +1,69 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['~','`','!', '@','#', '$', '%', '&', '(', ')', '*', '+',':',';','/','|','<','>','[',']','-']
+
+
+def genPassword():
+    letterPrompt = int(input("How many letters would you like in your password?\n"))
+    numberPrompt = int(input(f"How many numbers would you like?\n"))
+    symbolPrompt= int(input(f"How many symbols would you like?\n"))
+
+    password_list = []
+
+    for char in range(1, letterPrompt + 1):
+        password_list.append(random.choice(letters))
+
+    for char in range(1, numberPrompt + 1):
+        password_list += random.choice(numbers)
+
+    for char in range(1, symbolPrompt + 1):
+        password_list += random.choice(symbols)
+      
+    random.shuffle(password_list)
+   
+    password = ""
+    for char in password_list:
+        password += char
+
+    print(f"Your password is: {password}")  
+       
+# Menu
+print('''
+
+-----------------------PYTHON PASSWORD GENERATOR------------------------------
+	
+	Make a Choice to Begin! 
+	
+	1. Generate a Password
+	2. Quit
+	
+	''')
+userchoice = int(input("\n=>"))
+	
+if userchoice == 1:         
+    genPassword()
+
+    print(''' 
+    
+    Do you want to generate another password?
+
+    1. Yes, Please!
+    2. No, I'm Good :)
+
+    ''')
+    userchoice = int(input("\n=>"))
+
+    if userchoice == 1:
+        genPassword()
+                
+    else:
+        programmRunning = False
+        print("See you next time!")     
+
+elif userchoice == 2:
+		programmRunning = False
+		print("Bye!")
+else:
+		print("invalid entry!!")
+	
